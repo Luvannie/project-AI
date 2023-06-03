9cbded768b7636c628a5411cb009b8d9c18af903
Luvannie <100963246+Luvannie@users.noreply.github.com>
2023-06-03 11:01:08 +0700

    first version

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import googlemaps
+
+# Khóa API Google Maps
+api_key = 'AIzaSyD33vGmY4G4vlB1m4HNCcWPQ1uMGSZd3QI'
+
+# Tạo đối tượng client của Google Maps
+gmaps = googlemaps.Client(key=api_key)
+
+# Khởi tạo ứng dụng Flask
+app = Flask(__name__)
+
+# Route mặc định
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+# Route xử lý form submit
+@app.route('/directions', methods=['POST'])
+def directions():
+    origin = request.form['origin']
+    destination = request.form['destination']
+    
+    steps = find_directions(origin, destination)
+    
+    if steps:
+        return render_template("full-api\directions.html", steps=steps)
+    else:
+        return render_template("full-api\directions.html", error="Không tìm thấy đường đi.")
+
+# Tìm đường đi giữa hai điểm
+def find_directions(origin, destination):
+    directions_result = gmaps.directions(origin, destination, mode="driving")
+    if directions_result:
+        return directions_result[0]['legs'][0]['steps']
+    return None
+
+if __name__ == '__main__':
+    app.run()
